e0d1a261943f99533d52555d7060d88a89840398
Colin Graf <colin.graf@sovereign-labs.com>
2019-11-09 19:10:22 +0100

    Use CMake instead of mare

--- generate.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-if not exist Build\Debug\.mare\mare.exe call Ext\mare\compile.bat --buildDir=Build/Debug/.mare --outputDir=Build/Debug/.mare --sourceDir=Ext/mare/src
-if not "%1"=="" (Build\Debug\.mare\mare.exe %*) else Build\Debug\.mare\mare.exe --vcxproj=2013
